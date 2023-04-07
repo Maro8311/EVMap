2a0a3f3f6a0f5f2524cbc34108eec0ffc3ec3e2f
Maro8311 <43753598+Maro8311@users.noreply.github.com>
2023-04-07 20:27:23 +0200

    Caching in localStorage

--- components/LocationInput.jsx
+++ components/LocationInput.jsx
@@ -1,30 +1,49 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
-function LocationInput({location, setLocation, radius, setRadius, handleFormSubmit, handleReset}) {
+function LocationInput({ location, setLocation, radius, setRadius, handleFormSubmit, handleReset }) {
   const [suggestions, setSuggestions] = useState([]);
+  const cache = useMemo(() => ({}), []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     handleFormSubmit(e);
-  };
-
+  }; 
+  
   const handleLocationChange = async (event) => {
     setLocation(event.target.value);
-
-    if (event.target.value) {
-      try {
-        const response = await fetch(
-          `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(event.target.value)}&countrycodes=de&format=json`
-        );
-        const data = await response.json();
-        setSuggestions(data.map(result => result.display_name));
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
+    let tmpLocationName = event.target.value;
+    const query = event.target.value.trim().toLowerCase();
+  
+    // Check if data is already cached
+    const cachedData = localStorage.getItem(query);
+    if (cachedData) {
+      setSuggestions(JSON.parse(cachedData));
+      console.log(`Get cached: ${tmpLocationName} - ${radius} km`);
+      return;
+    }
+  
+    // Don't query data if locationInput is empty
+    if (!query) {
       setSuggestions([]);
+      return;
+    }
+  
+    const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(query)}&countrycodes=de&format=json`;
+    
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(`Query: ${tmpLocationName} - ${radius} km`);
+      const suggestions = data.map(result => result.display_name);
+      setSuggestions(suggestions);
+      // Store data in cache
+      localStorage.setItem(query, JSON.stringify(suggestions));
+    } catch (error) {
+      console.error(error);
     }
   };
+  
+
 
   const handleSuggestionClick = (suggestion) => {
     setLocation(suggestion);
@@ -35,12 +54,12 @@ function LocationInput({location, setLocation, radius, setRadius, handleFormSubm
     <form onSubmit={handleSubmit}>
       <div className="form-group">
         <label htmlFor="inputLocation">Location:</label>
-        <input 
-          id="inputLocation" 
-          type="text" 
-          value={location} 
-          onChange={handleLocationChange} 
-          autoComplete="off" 
+        <input
+          id="inputLocation"
+          type="text"
+          value={location}
+          onChange={handleLocationChange}
+          autoComplete="off"
         />
         {suggestions.length > 0 && (
           <ul className='ui-autocomplete'>
